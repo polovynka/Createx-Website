78d73f130a2436b0570ab29a1ab5448d01bc91b2
Vladyslav Polovynka <polovynka.dev@gmail.com>
2021-12-18 22:14:16 +0200

    add facts section

---
+++ src/js/components/facts-circle.js
@@ -0,0 +1,34 @@
+const circle = document.querySelectorAll('svg[data-value] .circle');
+
+// Calc value of strokeDashoffset
+
+function calcStrokeDashoffset() {
+	circle.forEach((element) => {
+		let length = element.getTotalLength();
+
+		let value = parseInt(element.parentNode.getAttribute('data-value'));
+
+		let to = length * ((100 - value) / 100);
+
+		element.style.strokeDashoffset = Math.max(0, to);
+	});
+};
+
+// Observer
+
+const observer = new IntersectionObserver(entries => {
+
+	entries.forEach(entry => {
+
+		if (entry.isIntersecting) {
+
+			calcStrokeDashoffset();
+
+		}
+
+	});
+
+});
+
+observer.observe(document.querySelector('.facts__list'));
+
